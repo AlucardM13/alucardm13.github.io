ebe2e76b5afa88061a16b8fc9e34b351454fbb6d
AlucardM13 <mr6174@nyu.edu>
2022-09-21 16:54:07 +0400

    uploading website

---
+++ script.js
@@ -0,0 +1,11 @@
+function background1() {
+        document.querySelector('body').style.background = "url('background5.jpg') center center / cover";
+    }
+    function background2() {
+        document.querySelector('body').style.background = "url('bckground.jpg') center center / cover";
+    }
+    function background3() {
+        document.querySelector('body').style.background = "url('bckground3.jpg') center center / cover";
+    }
+
+    // adding functionality to buttons
